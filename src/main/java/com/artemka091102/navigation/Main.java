a320872a3bdffda1d5fa13c1d4acfbd9a92da9ad
artemka091102 <Ponomarev.Artem02@gmail.com>
2020-05-06 18:41:19 +0300

    started making magnesite item

--- src/main/java/com/artemka091102/navigation/Main.java
+++ src/main/java/com/artemka091102/navigation/Main.java
@@ -1,8 +1,19 @@
 package com.artemka091102.navigation;
 
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(Main.MODID)
 public class Main {
     public static final String MODID = "navigation";
+
+    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+    public static class Registry{
+        @SubscribeEvent
+        public static void registerItems(final RegistryEvent.Register<Item> event) {
+
+        }
+    }
 }
---
+++ src/main/java/com/artemka091102/navigation/lists/ItemList.java
@@ -0,0 +1,7 @@
+package com.artemka091102.navigation.lists;
+
+import net.minecraft.item.Item;
+
+public class ItemList {
+    public static Item magnesite;
+}
